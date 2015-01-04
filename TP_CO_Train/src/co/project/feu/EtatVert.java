1b0ff236b08db5deace01d679aa196972e268bde
guillaume.blanchard37 <guillaume.blanchard37@gmail.com>
2015-01-04 09:54:00 +0000

    passage des classes Etat en singleton, pour garantir l'unique instance

--- TP_CO_Train/src/co/project/feu/EtatVert.java
+++ TP_CO_Train/src/co/project/feu/EtatVert.java
@@ -1,18 +1,32 @@
 package co.project.feu;
 
-public class EtatVert extends EtatFeu{
+public class EtatVert extends EtatFeu {
 
-	public EtatVert() {
-		super("vert");
+	/* Unique instance non initialisee */
+	private static EtatVert INSTANCE = null;
+
+	// Constructeur prive
+	private EtatVert() {
+		super("rouge");
+	}
+
+	/**
+	 * @return unique point d'acces du singleton reseau
+	 */
+	public static synchronized EtatVert getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new EtatVert();
+		}
+		return INSTANCE;
 	}
 
 	@Override
 	public EtatFeu changeEtat(Semaphore sema) {
 		// TODO Auto-generated method stub
-		if(sema.changementEtatPossible(new EtatOrange()))
-			return new EtatOrange();
+		if (sema.changementEtatPossible(EtatOrange.getInstance()))
+			return EtatOrange.getInstance();
 		else
-			return new EtatRouge();
+			return EtatRouge.getInstance();
 	}
 
 }
--- TP_CO_Train/src/co/project/feu/FeuBicolore.java
+++ TP_CO_Train/src/co/project/feu/FeuBicolore.java
@@ -2,12 +2,11 @@ package co.project.feu;
 
 public class FeuBicolore extends Semaphore {
 
-
 	public FeuBicolore() {
 		// TODO Auto-generated constructor stub
 		super();
 	}
-	
+
 	@Override
 	public void changeEtat() {
 		// TODO Auto-generated method stub
@@ -22,18 +21,17 @@ public class FeuBicolore extends Semaphore {
 	@Override
 	public boolean changementEtatPossible(EtatFeu etat) {
 		// TODO Auto-generated method stub
-		
-		if(etat.getName().equals("orange"))
+
+		if (etat.getName().equals("orange"))
 			return false;
-		
+
 		return true;
 	}
-	
+
 	public static void main(String[] args) {
 		FeuBicolore bico = new FeuBicolore();
-		
-		System.out.println("Changement possible ? "+bico.changementEtatPossible(new EtatRouge()));
+
+		System.out.println("Changement possible ? " + bico.changementEtatPossible(EtatRouge.getInstance()));
 	}
 
-	
 }
